084f1f51cc63fef6bd0ab5fe93db03757017b407
Ruslan <ruslanmehdiyev922@gmail.com>
2023-01-06 13:57:45 +0400

    saved

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import WindowSize from "./components/WindowSize";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Window size</h2>
+      <WindowSize />
+    </div>
+  );
+}
+
+export default App;
